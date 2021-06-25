dcc924f726ec98df295fcf5927c16f9dd4963777
Craig <indigomx9@gmail.com>
2021-06-25 17:08:43 -0400

    I added TailwindCSS to the project.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+// import "tailwindcss/tailwind.css";
 
 export default function 
 App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Index() {
     return (
         <React.Fragment>
-            <h1>Query</h1>
+            <h1 className="backdrop-filter 
+                bg-purple-500">Query</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nostrum. Architecto molestiae perspiciatis tenetur tempore accusamus praesentium ad eaque deleniti dolores, odio nesciunt explicabo earum. Culpa quae commodi non minus?</p>
         </React.Fragment>
     );
